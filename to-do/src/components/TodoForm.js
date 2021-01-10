55b2619d857a51225019f9a9b9ca579efbcafd38
zachisaacson <zachary.isaacson@wustl.edu>
2021-01-10 14:00:40 -0500

    complete functionality for adding and deleting todos & styling

---
+++ to-do/src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] =useState('');
+
+    const handleChange = text =>{
+        setInput(text.target.value);
+    }
+
+    const handleSubmit = e=>{
+        e.preventDefault();
+        props.onSubmit({
+            id: Math.floor(Math.random()*100000),
+            text: input
+        })
+        setInput('');
+    }
+
+    return (
+        <form className='todo-form' onSubmit={handleSubmit}>
+            <input type="text" placeholder="Add a todo" value={input}  name="text" className="todo-input" onChange={handleChange}/>
+            <button className="todo-button"> Add Todo</button>
+        </form>
+    )
+}
+
+export default TodoForm
